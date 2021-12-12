a48528fc3951ac8b5e77f4e06df5e39db9e3cc54
Mykhailo Hnap <mike.gnap.r@gmail.com>
2021-12-12 21:31:24 +0200

    Added global initialization practice.

---
+++ src/practice/global_initialization.cpp
@@ -0,0 +1,15 @@
+// https://stackoverflow.com/questions/8708539/order-of-initialization
+
+// If uncomment this, std::cout will be initialized before usage
+//#include <iostream>
+
+struct Foo {
+  Foo();
+} foo;
+
+// In this case, std::cout won't be initialized and program will catch SIGSEGV
+#include <iostream>
+
+Foo::Foo() { std::cout << "Hello World"; }
+
+int main() {}
