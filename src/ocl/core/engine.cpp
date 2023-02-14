e95621569d97a4e56d174fbf694a189b79dc4ff4
Mykhailo Hnap <mike.gnap.r@gmail.com>
2023-02-14 17:57:54 +0200

    Fixed clang-tidy warnings in ocl engine.

--- src/ocl/core/engine.cpp
+++ src/ocl/core/engine.cpp
@@ -58,75 +58,75 @@ void Engine::addCompilerOptionIncludeDirectory(std::string_view dir) {
 void Engine::run() {
   // Get platform id
   auto platformId = std::make_unique<cl_platform_id>();
-  int err = clGetPlatformIDs(1, platformId.get(), NULL);
+  int err = clGetPlatformIDs(1, platformId.get(), nullptr);
   if (err != CL_SUCCESS) {
     throw OpenCLError("Failed to get platform id", err);
   }
 
   // Connect to a compute device
-  cl_device_id deviceId;
-  int gpu = 1;
-  err = clGetDeviceIDs(*platformId, gpu ? CL_DEVICE_TYPE_GPU : CL_DEVICE_TYPE_CPU, 1, &deviceId, NULL);
+  cl_device_id deviceId = nullptr;
+  const bool gpu = true;
+  err = clGetDeviceIDs(*platformId, gpu ? CL_DEVICE_TYPE_GPU : CL_DEVICE_TYPE_CPU, 1, &deviceId, nullptr);
   if (err != CL_SUCCESS) {
     throw OpenCLError("Failed to create a device group", err);
   }
 
   // Create a compute context
-  cl_context context = clCreateContext(0, 1, &deviceId, NULL, NULL, &err);
-  if (!context) {
+  cl_context context = clCreateContext(nullptr, 1, &deviceId, nullptr, nullptr, &err);
+  if (context == nullptr) {
     throw OpenCLError("Failed to create a compute context", err);
   }
 
   // Create a command queue
   cl_command_queue commands = clCreateCommandQueue(context, deviceId, 0, &err);
-  if (!commands) {
+  if (commands == nullptr) {
     throw OpenCLError("Failed to create a command queue", err);
   }
 
   // Load kernel source from file
-  std::string kernelSource = loadKernelSource();
+  const std::string kernelSource = loadKernelSource();
   const char* kernelSourcePtr = kernelSource.c_str();
 
   // Create compute program from the source buffer
-  cl_program program = clCreateProgramWithSource(context, 1, (const char**)&kernelSourcePtr, NULL, &err);
-  if (!program) {
+  cl_program program = clCreateProgramWithSource(context, 1, (const char**)&kernelSourcePtr, nullptr, &err);
+  if (program == nullptr) {
     throw OpenCLError("Failed to create compute program", err);
   }
 
   // Build the program executable
-  err = clBuildProgram(program, 0, NULL, m_compilerOptions.c_str(), NULL, NULL);
+  err = clBuildProgram(program, 0, nullptr, m_compilerOptions.c_str(), nullptr, nullptr);
   if (err != CL_SUCCESS) {
     // Determine the size of the log
-    size_t log_size;
-    clGetProgramBuildInfo(program, deviceId, CL_PROGRAM_BUILD_LOG, 0, NULL, &log_size);
+    size_t log_size = 0;
+    clGetProgramBuildInfo(program, deviceId, CL_PROGRAM_BUILD_LOG, 0, nullptr, &log_size);
     // Allocate memory for the log
     std::string log;
     log.resize(log_size / sizeof(std::string::value_type));
     // Get the log
-    clGetProgramBuildInfo(program, deviceId, CL_PROGRAM_BUILD_LOG, log_size, log.data(), NULL);
+    clGetProgramBuildInfo(program, deviceId, CL_PROGRAM_BUILD_LOG, log_size, log.data(), nullptr);
     throw OpenCLError("Failed to build program executable:\n" + std::string(log), err);
   }
 
   // Create compute kernel in the program we wish to run
   cl_kernel kernel = clCreateKernel(program, m_kernelName.data(), &err);
-  if (!kernel || err != CL_SUCCESS) {
+  if ((kernel == nullptr) || (err != CL_SUCCESS)) {
     throw OpenCLError("Failed to create compute kernel", err);
   }
 
-  cl_mem inputMem; // device memory used for the input array
-  cl_mem outputMem; // device memory used for the output array
+  cl_mem inputMem = nullptr; // device memory used for the input array
+  cl_mem outputMem = nullptr; // device memory used for the output array
 
   if (m_data) {
-    size_t dataSize = dataTypeToSize(m_data->type) * m_data->size;
+    const size_t dataSize = dataTypeToSize(m_data->type) * m_data->size;
     // Create the input and output arrays in device memory for our calculation
-    inputMem = clCreateBuffer(context, CL_MEM_READ_ONLY, dataSize, NULL, NULL);
-    outputMem = clCreateBuffer(context, CL_MEM_WRITE_ONLY, dataSize, NULL, NULL);
-    if (!inputMem || !outputMem) {
+    inputMem = clCreateBuffer(context, CL_MEM_READ_ONLY, dataSize, nullptr, nullptr);
+    outputMem = clCreateBuffer(context, CL_MEM_WRITE_ONLY, dataSize, nullptr, nullptr);
+    if ((inputMem == nullptr) || (outputMem == nullptr)) {
       throw OpenCLError("Failed to allocate device memory", err);
     }
 
     // Write our data set into the inputMem array in device memory
-    err = clEnqueueWriteBuffer(commands, inputMem, CL_TRUE, 0, dataSize, m_data->input, 0, NULL, NULL);
+    err = clEnqueueWriteBuffer(commands, inputMem, CL_TRUE, 0, dataSize, m_data->input, 0, nullptr, nullptr);
     if (err != CL_SUCCESS) {
       throw OpenCLError("Failed to write to source array", err);
     }
@@ -142,9 +142,10 @@ void Engine::run() {
 
   // Execute the kernel over the entire range of our 1d inputMem data set
   // using the maximum number of work group items for this device
-  err = clEnqueueNDRangeKernel(commands, kernel, m_globalWorkSizes.size(), NULL, m_globalWorkSizes.data(),
-                               m_localWorkSizes.empty() ? NULL : m_localWorkSizes.data(), 0, NULL, NULL);
-  if (err) {
+  err = clEnqueueNDRangeKernel(commands, kernel, m_globalWorkSizes.size(), nullptr, m_globalWorkSizes.data(),
+                               m_localWorkSizes.empty() ? nullptr : m_localWorkSizes.data(), 0, nullptr,
+                               nullptr);
+  if (err != CL_SUCCESS) {
     throw OpenCLError("Failed to execute kernel", err);
   }
 
@@ -152,9 +153,9 @@ void Engine::run() {
   clFinish(commands);
 
   if (m_data) {
-    size_t dataSize = dataTypeToSize(m_data->type) * m_data->size;
+    const size_t dataSize = dataTypeToSize(m_data->type) * m_data->size;
     // Read back the results from the device to verify the outputMem
-    err = clEnqueueReadBuffer(commands, outputMem, CL_TRUE, 0, dataSize, m_data->output, 0, NULL, NULL);
+    err = clEnqueueReadBuffer(commands, outputMem, CL_TRUE, 0, dataSize, m_data->output, 0, nullptr, nullptr);
     if (err != CL_SUCCESS) {
       throw OpenCLError("Failed to read output array", err);
     }
