d4dd95e1567092afe43774d84da55e231ac13dbd
Mykhailo Hnap <mike.gnap.r@gmail.com>
2023-03-04 15:11:59 +0200

    Added functions that return parameter to copy elision lesson.

--- src/lessons/copy_elision.cpp
+++ src/lessons/copy_elision.cpp
@@ -15,25 +15,52 @@ common::Object wrongNRVO() {
   return std::move(obj); // cannot do NRVO
 }
 
+common::Object getFromParameter(common::Object obj) { return obj; }
+common::Object getFromRefParameter(const common::Object& obj) { return obj; }
+
 void printObjectAddress(common::Object obj) { common::println("&obj=", &obj); }
 
 int main() {
   {
     common::println("----- RVO -----");
     common::Object obj = RVO(); // prvalue, copy elision
-    printObjectAddress(std::move(obj)); // xvalue, no copy elision
-    printObjectAddress(RVO()); // prvalue, copy elision
   }
   {
     common::println("----- Correct NRVO -----");
     common::Object obj = correctNRVO(); // prvalue, copy elision
-    printObjectAddress(std::move(obj)); // xvalue, no copy elision
-    printObjectAddress(correctNRVO()); // prvalue, copy elision
   }
   {
     common::println("----- Wrong NRVO -----");
     common::Object obj = wrongNRVO(); // prvalue, copy elision
-    printObjectAddress(std::move(obj)); // xvalue, no copy elision
-    printObjectAddress(wrongNRVO()); // prvalue, copy elision
+  }
+  {
+    common::println("----- Initialization copy elision -----");
+    printObjectAddress(RVO());
+  }
+  {
+    common::println("----- Get from parameter -----");
+    common::Object obj;
+    // Object is copied as function parameter and then this new copy is moved by compiler as return value
+    common::println("lvalue:");
+    getFromParameter(obj);
+    // Object is moved as function parameter and then this new copy is moved by compiler as return value
+    common::println("xvalue:");
+    getFromParameter(std::move(obj));
+    // Copy elision for function parameter and then object is moved by compiler as return value
+    common::println("prvalue:");
+    getFromParameter(common::Object());
+  }
+  {
+    common::println("----- Get from ref parameter -----");
+    common::Object obj;
+    // Object is copied as return value
+    common::println("lvalue:");
+    getFromRefParameter(obj);
+    // Object is copied as return value
+    common::println("xvalue:");
+    getFromRefParameter(std::move(obj));
+    // Object is copied as return value
+    common::println("prvalue:");
+    getFromRefParameter(common::Object());
   }
 }
