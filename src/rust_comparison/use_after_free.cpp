f3e475bd1cb3d5fbe58a975e78cc73ee8de02627
Mykhailo Hnap <mike.gnap.r@gmail.com>
2023-04-05 13:42:57 +0300

    Fixed formatting.

--- src/rust_comparison/use_after_free.cpp
+++ src/rust_comparison/use_after_free.cpp
@@ -16,17 +16,11 @@ int main(int argc, const char** argv) {
 }
 
 // =================================================================
-//==201660==ERROR: AddressSanitizer: heap-use-after-free on address 0x60d000000040 at pc 0x55c27327b77e bp
-// 0x7ffd9a089cf0 sp 0x7ffd9a0894c0 READ of size 72 at 0x60d000000040 thread T0
-//==201660==WARNING: invalid path to external symbolizer!
-//==201660==WARNING: Failed to use and restart external symbolizer!
-//    #0 0x55c27327b77d  (/home/mhnap/projects/DIY/build/debug/src/rust_comparison/use_after_free+0x3b77d)
-//    (BuildId: ee3566d5097b4d4af9b1763ce6059caa09740461) #1 0x7f8d5113cb34
-//    (/lib/x86_64-linux-gnu/libstdc++.so.6+0x13cb34) (BuildId: f57e02bfadacc0c923c82457d5e18e1830b5faea) #2
-//    0x55c27331f8dc  (/home/mhnap/projects/DIY/build/debug/src/rust_comparison/use_after_free+0xdf8dc)
-//    (BuildId: ee3566d5097b4d4af9b1763ce6059caa09740461) #3 0x7f8d50c29d8f
-//    (/lib/x86_64-linux-gnu/libc.so.6+0x29d8f) (BuildId: 69389d485a9793dbe873f0ea2c93e02efaa9aa3d) #4
-//    0x7f8d50c29e3f  (/lib/x86_64-linux-gnu/libc.so.6+0x29e3f) (BuildId:
-//    69389d485a9793dbe873f0ea2c93e02efaa9aa3d) #5 0x55c27325f454
-//    (/home/mhnap/projects/DIY/build/debug/src/rust_comparison/use_after_free+0x1f454) (BuildId:
-//    ee3566d5097b4d4af9b1763ce6059caa09740461)
+//==1916738==ERROR: AddressSanitizer: heap-use-after-free on address 0x60c000000040 at pc 0x7f3556e4e602 bp 0x7fffa564b040 sp 0x7fffa564a7e8
+//READ of size 59 at 0x60c000000040 thread T0
+//    #0 0x7f3556e4e601 in __interceptor_fwrite ../../../../src/libsanitizer/sanitizer_common/sanitizer_common_interceptors.inc:1157
+//    #1 0x7f3556b3cb34 in std::basic_ostream<char, std::char_traits<char> >& std::__ostream_insert<char, std::char_traits<char> >(std::basic_ostream<char, std::char_traits<char> >&, char const*, long) (/lib/x86_64-linux-gnu/libstdc++.so.6+0x13cb34)
+//    #2 0x5569f8e777eb in main /home/mhnap/projects/DIY/src/rust_comparison/use_after_free.cpp:15
+//    #3 0x7f3556629d8f in __libc_start_call_main ../sysdeps/nptl/libc_start_call_main.h:58
+//    #4 0x7f3556629e3f in __libc_start_main_impl ../csu/libc-start.c:392
+//    #5 0x5569f8e774c4 in _start (/home/mhnap/projects/DIY/build/debug/src/rust_comparison/rust_comp_use_after_free+0x34c4)
