640e771ffa2f6f3efabc25ed0e620723de809aa4
Mykhailo Hnap <mike.gnap.r@gmail.com>
2024-05-17 12:30:03 +0300

    More about panic = 'abort'

--- rust/others/books/trpl/src/bin/error_handling.rs
+++ rust/others/books/trpl/src/bin/error_handling.rs
@@ -48,7 +48,18 @@ fn main() -> Result<(), Box<dyn Error>> {
     // In both cases, we cause a panic in our program.
     // By default, these panics will print a failure message, unwind, clean up the stack, and quit.
     // Via an environment variable, you can also have Rust display the call stack when a panic occurs to make it easier to track down the source of the panic.
-    // Default `unwind` mode when panicking can be changed to `abort` mode in profile section in Cargo.toml file.
+
+    // By default, when a panic occurs, the program starts unwinding, which means Rust walks back up the stack and cleans up the data from each function it encounters.
+    // However, this walking back and cleanup is a lot of work.
+    // Rust, therefore, allows you to choose the alternative of immediately aborting, which ends the program without cleaning up.
+
+    // Memory that the program was using will then need to be cleaned up by the operating system.
+    // If in your project you need to make the resulting binary as small as possible, you can switch from unwinding to aborting upon a panic by adding panic = 'abort' to the appropriate [profile] sections in your Cargo.toml file.
+    // For example, if you want to abort on panic in release mode, add this:
+    // [profile.release]
+    // panic = 'abort'
+
+    // Note: in such case, drop(s) won't be called, as the are called during stack unwinding that won't happen.
 
     // Will call drop during stack unwinding.
     struct A {}
