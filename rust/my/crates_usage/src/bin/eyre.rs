8a2eb3830c8fda4aad2b730cad92cca403781860
Mykhailo Hnap <mike.gnap.r@gmail.com>
2024-08-28 14:04:15 +0300

    Add more to eyre

--- rust/my/crates_usage/src/bin/eyre.rs
+++ rust/my/crates_usage/src/bin/eyre.rs
@@ -2,11 +2,18 @@ use color_eyre::eyre::Context;
 
 macro_rules! print_err {
     ($err:expr) => {
-        eprintln!("----- {} at {} -----", stringify!($err), line!());
+        eprintln!(
+            "----- {} at {}:{}:{} -----",
+            stringify!($err),
+            file!(),
+            line!(),
+            column!()
+        );
         eprintln!("Display:\n{}", $err);
         eprintln!("Display alternate:\n{:#}", $err);
         eprintln!("Debug:\n{:?}", $err);
         eprintln!("Debug alternate:\n{:#?}", $err);
+        eprintln!("----------------------");
     };
 }
 
@@ -53,12 +60,12 @@ fn main() {
     //
 
     {
-        // Can be used to save the original error together with backtrace and location.
+        // Can be used to save the original error together with location and location.
 
         #[derive(Debug, thiserror::Error)]
         enum MyError {
             #[error(transparent)]
-            Io(color_eyre::eyre::Error),
+            Io(color_eyre::Report),
         }
 
         impl From<std::io::Error> for MyError {
@@ -69,7 +76,7 @@ fn main() {
         }
 
         impl MyError {
-            fn as_eyre_err(&self) -> Option<&color_eyre::eyre::Error> {
+            fn as_eyre_err(&self) -> Option<&color_eyre::Report> {
                 match self {
                     MyError::Io(err) => Some(err),
                     _ => None,
@@ -98,5 +105,257 @@ fn main() {
                 }
             }
         }
+    }
+
+    //
+
+    // Can get external error with location.
+
+    {
+        // External error from some sub-crate we nothing knows about.
+        #[derive(Debug, thiserror::Error)]
+        enum ExternalError {
+            #[error(transparent)]
+            Io(color_eyre::Report),
+
+            #[error("Other error")]
+            Other,
+        }
+
+        impl From<std::io::Error> for ExternalError {
+            #[track_caller]
+            fn from(value: std::io::Error) -> Self {
+                Self::Io(value.into())
+            }
+        }
+
+        fn read_two_files(
+            path1: &std::path::Path,
+            path2: &std::path::Path,
+        ) -> Result<Vec<u8>, ExternalError> {
+            let mut buf1 = std::fs::read(path1)?;
+            let buf2 = std::fs::read(path2)?;
+            buf1.extend(buf2);
+            Ok(buf1)
+        }
+
+        // Our own error.
+        #[derive(Debug, thiserror::Error)]
+        enum MyError {
+            // Variant for storing external error.
+            #[error(transparent)]
+            External(#[from] ExternalError),
+        }
+
+        fn my_read_two_files() -> Result<Vec<u8>, MyError> {
+            Ok(read_two_files("file1".as_ref(), "file2".as_ref())?)
+        }
+
+        let res = my_read_two_files();
+        match res {
+            Ok(buf) => println!("We read buf: {buf:?}"),
+            Err(err) => {
+                print_err!(err);
+            }
+        }
+
+        // Location:
+        //  my/crates_usage/src/bin/eyre.rs:136
+
+        fn other() -> Result<(), ExternalError> {
+            Err(ExternalError::Other)
+        }
+
+        fn my_other() -> Result<(), MyError> {
+            Ok(other()?)
+        }
+
+        let res = my_other();
+        match res {
+            Ok(()) => println!("Success!"),
+            Err(err) => {
+                print_err!(err);
+            }
+        }
+
+        // But in such case there would be no location.
+    }
+
+    //
+
+    // But if our error also wants to use location, only it would be used, and not from external error.
+
+    {
+        // External error from some sub-crate we nothing knows about.
+        #[derive(Debug, thiserror::Error)]
+        enum ExternalError {
+            #[error(transparent)]
+            Io(color_eyre::Report),
+
+            #[error("Other error")]
+            Other,
+        }
+
+        impl From<std::io::Error> for ExternalError {
+            #[track_caller]
+            fn from(value: std::io::Error) -> Self {
+                Self::Io(value.into())
+            }
+        }
+
+        fn read_two_files(
+            path1: &std::path::Path,
+            path2: &std::path::Path,
+        ) -> Result<Vec<u8>, ExternalError> {
+            let mut buf1 = std::fs::read(path1)?;
+            let buf2 = std::fs::read(path2)?;
+            buf1.extend(buf2);
+            Ok(buf1)
+        }
+
+        // Our own error.
+        #[derive(Debug, thiserror::Error)]
+        enum MyError {
+            // Variant for storing external error.
+            #[error(transparent)]
+            External(color_eyre::Report),
+        }
+
+        impl From<ExternalError> for MyError {
+            #[track_caller]
+            fn from(value: ExternalError) -> Self {
+                Self::External(value.into())
+            }
+        }
+
+        fn my_read_two_files() -> Result<Vec<u8>, MyError> {
+            Ok(read_two_files("file1".as_ref(), "file2".as_ref())?)
+        }
+
+        let res = my_read_two_files();
+        match res {
+            Ok(buf) => println!("We read buf: {buf:?}"),
+            Err(err) => {
+                print_err!(err);
+            }
+        }
+
+        // Location:
+        //  my/crates_usage/src/bin/eyre.rs:232
+
+        fn other() -> Result<(), ExternalError> {
+            Err(ExternalError::Other)
+        }
+
+        fn my_other() -> Result<(), MyError> {
+            Ok(other()?)
+        }
+
+        let res = my_other();
+        match res {
+            Ok(()) => println!("Success!"),
+            Err(err) => {
+                print_err!(err);
+            }
+        }
+
+        // Location:
+        //  my/crates_usage/src/bin/eyre.rs:251
+    }
+
+    //
+
+    // But we can add some manual logic to handle this.
+
+    {
+        // External error from some sub-crate we nothing knows about.
+        #[derive(Debug, thiserror::Error)]
+        enum ExternalError {
+            #[error(transparent)]
+            Io(color_eyre::Report),
+
+            #[error("Other error")]
+            Other,
+        }
+
+        impl From<std::io::Error> for ExternalError {
+            #[track_caller]
+            fn from(value: std::io::Error) -> Self {
+                Self::Io(value.into())
+            }
+        }
+
+        impl ExternalError {
+            fn into_eyre_report(self) -> Result<color_eyre::Report, Self> {
+                match self {
+                    ExternalError::Io(eyre_report) => Ok(eyre_report),
+                    other_err => Err(other_err),
+                }
+            }
+        }
+
+        fn read_two_files(
+            path1: &std::path::Path,
+            path2: &std::path::Path,
+        ) -> Result<Vec<u8>, ExternalError> {
+            let mut buf1 = std::fs::read(path1)?;
+            let buf2 = std::fs::read(path2)?;
+            buf1.extend(buf2);
+            Ok(buf1)
+        }
+
+        // Our own error.
+        #[derive(Debug, thiserror::Error)]
+        enum MyError {
+            // Variant for storing external error.
+            #[error(transparent)]
+            External(color_eyre::Report),
+        }
+
+        impl From<ExternalError> for MyError {
+            #[track_caller]
+            fn from(value: ExternalError) -> Self {
+                match value.into_eyre_report() {
+                    Ok(eyre_report) => Self::External(eyre_report),
+                    Err(other_err) => Self::External(other_err.into()),
+                }
+            }
+        }
+
+        fn my_read_two_files() -> Result<Vec<u8>, MyError> {
+            Ok(read_two_files("file1".as_ref(), "file2".as_ref())?)
+        }
+
+        let res = my_read_two_files();
+        match res {
+            Ok(buf) => println!("We read buf: {buf:?}"),
+            Err(err) => {
+                print_err!(err);
+            }
+        }
+
+        // We get the original error and location.
+        //
+        // Location:
+        //  my/crates_usage/src/bin/eyre.rs:301
+
+        fn other() -> Result<(), ExternalError> {
+            Err(ExternalError::Other)
+        }
+
+        fn my_other() -> Result<(), MyError> {
+            Ok(other()?)
+        }
+
+        let res = my_other();
+        match res {
+            Ok(()) => println!("Success!"),
+            Err(err) => {
+                print_err!(err);
+            }
+        }
+
+        // Location:
+        //  my/crates_usage/src/bin/eyre.rs:347
     }
 }
