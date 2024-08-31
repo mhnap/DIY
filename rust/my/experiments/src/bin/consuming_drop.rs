ee15fc3b2769fede48fdb0f664fb1353e849ce46
Mykhailo Hnap <mike.gnap.r@gmail.com>
2024-08-31 22:01:45 +0300

    Add consuming drop

---
+++ rust/my/experiments/src/bin/consuming_drop.rs
@@ -0,0 +1,22 @@
+// https://langdev.stackexchange.com/questions/3926/in-rust-why-did-the-designers-choose-to-make-drop-take-mut-self-instead-of-sel
+// https://stackoverflow.com/questions/30905826/why-does-drop-take-mut-self-instead-of-self
+// https://github.com/rust-lang/rust/issues/4330
+
+fn main() {
+    struct Foo {
+        // ... some fields
+    }
+
+    fn do_something(foo: Foo) {
+        // ... some code
+        foo;
+    }
+
+    impl Drop for Foo {
+        fn drop(self) {
+            do_something(self);
+        }
+    }
+
+    let foo = Foo {};
+}
