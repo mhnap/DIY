3ff5dd910dcb34abbdfd5aaedfb0de165f9764fa
Mykhailo Hnap <mike.gnap.r@gmail.com>
2024-09-05 14:31:38 +0300

    Add more to `Default` trait

--- rust/others/books/trpl/src/bin/default_trait.rs
+++ rust/others/books/trpl/src/bin/default_trait.rs
@@ -1,31 +1,40 @@
 fn main() {
-    // Need to manually specify all fields without Default trait.
+    // Need to manually specify all fields without `Default` trait.
     #[derive(Debug)]
     struct WithoutDefault {
         first: i32,
         second: i32,
     }
+
     let a1 = WithoutDefault {
         first: 0,
         second: 0,
     };
     dbg!(&a1);
 
-    // Or we can derive Default trait and use default method.
+    //
+
+    // Or we can derive `Default` trait and use the `default` method.
     #[derive(Debug, Default)]
     struct WithDerivedDefault {
         first: i32,
         second: i32,
     }
+
     let a2 = WithDerivedDefault::default();
     dbg!(&a2);
+
+    //
 
     // Also we can override a particular field, but still retain the other defaults.
     let a3: WithDerivedDefault = WithDerivedDefault {
         second: 42,
-        ..WithDerivedDefault::default()
+        ..WithDerivedDefault::default() // or ..Default::default()
+                                        // or ..<_>::default()
     };
     dbg!(&a3);
+
+    //
 
     // There is a special syntax to choose the enum default value.
     #[derive(Debug, Default)]
@@ -35,10 +44,37 @@ fn main() {
         B,
         C,
     }
+
     let a4 = Kind::default();
     dbg!(&a4);
 
-    // Need to implement Default trait for type to add custom default values.
+    //
+
+    // But it works only for unit enum variants.
+    // #[derive(Debug, Default)]
+    // enum Kind2 {
+    //     A,
+    //     #[default]
+    //     B {
+    //         i: i32,
+    //     },
+    //     C,
+    // }
+    //
+    // let a4 = Kind2::default();
+    // dbg!(&a4);
+    //
+    // error: the `#[default]` attribute may only be used on unit enum variants
+    //   --> others/books/trpl/src/bin/default_trait.rs:46:9
+    //    |
+    // 46 |         B {
+    //    |         ^
+    //    |
+    //    = help: consider a manual implementation of `Default`
+
+    //
+
+    // Need to implement `Default` trait for type to add custom default values.
     #[derive(Debug)]
     struct WithImplDefault {
         first: i32,
@@ -54,11 +90,13 @@ fn main() {
         }
     }
 
-    // Note, we can use such syntax with type annotation and Default trait.
+    // Note, we can use such syntax with type annotation and `Default` trait.
     let a5: WithImplDefault = Default::default();
     dbg!(&a5);
 
-    // One often usage of Default is in Option::unwrap_or_default method.
+    //
+
+    // One often usage of `Default` is in Option::unwrap_or_default method.
     let op = Option::<Kind>::None;
     dbg!(&op);
     dbg!(&op.unwrap_or_default());
