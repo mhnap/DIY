7d31f9ffccd41b93a1ebce5c51bb00241ddcee72
Mykhailo Hnap <mike.gnap.r@gmail.com>
2024-10-10 12:36:02 +0300

    Print request in web_server

--- rust/others/books/trpl/src/bin/web_server/main.rs
+++ rust/others/books/trpl/src/bin/web_server/main.rs
@@ -30,7 +30,15 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request: {http_request:#?}");
+
+    let request_line = http_request.first().unwrap();
 
     let (status_line, filename) = match request_line.as_str() {
         "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
