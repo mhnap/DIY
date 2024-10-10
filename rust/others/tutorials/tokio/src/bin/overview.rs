155b553115233e172d601ec08a7597aa5181c579
Mykhailo Hnap <mike.gnap.r@gmail.com>
2024-10-10 20:15:08 +0300

    Mock tokio tutorial

---
+++ rust/others/tutorials/tokio/src/bin/overview.rs
@@ -0,0 +1,14 @@
+// https://tokio.rs/tokio/tutorial
+
+fn main() {
+    // Tokio is an asynchronous runtime for the Rust programming language.
+    // It provides the building blocks needed for writing networking
+    // applications. It gives the flexibility to target a wide range of
+    // systems, from large servers with dozens of cores to small embedded
+    // devices.
+
+    // At a high level, Tokio provides a few major components:
+    // - A multi-threaded runtime for executing asynchronous code.
+    // - An asynchronous version of the standard library.
+    // - A large ecosystem of libraries.
+}
