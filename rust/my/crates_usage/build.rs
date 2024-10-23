1abbea884e4bc3d7047a90e11300265b542ab3d9
Mykhailo Hnap <mike.gnap.r@gmail.com>
2024-10-23 18:15:14 +0300

    Add prost crate usage

---
+++ rust/my/crates_usage/build.rs
@@ -0,0 +1,3 @@
+fn main() {
+    prost_build::compile_protos(&["src/bin/prost/person.proto"], &["src"]).unwrap();
+}
---
+++ rust/my/crates_usage/src/bin/prost/main.rs
@@ -0,0 +1,22 @@
+mod generated {
+    include!(concat!(env!("OUT_DIR"), "/_.rs"));
+}
+
+use crate::generated::Person;
+use prost::Message;
+
+fn main() {
+    let person =
+        Person { name: "Alice".to_string(), id: 123, email: Some("alice@example.com".to_string()) };
+
+    // Serialize the person message to bytes
+    let bytes = person.encode_to_vec();
+    dbg!(bytes.len());
+
+    // Deserialize the bytes back into a Person message
+    let deserialized_person: Person = Person::decode(bytes.as_slice()).unwrap();
+
+    dbg!(&deserialized_person.name);
+    dbg!(&deserialized_person.id);
+    dbg!(&deserialized_person.email);
+}
