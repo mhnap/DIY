28246c08357deda980bb7fe6741a06489d293bf7
Mykhailo Hnap <mike.gnap.r@gmail.com>
2026-02-15 19:39:55 +0000

    add AssertJson trait

--- rust/my/crates_usage/src/bin/expect-test.rs
+++ rust/my/crates_usage/src/bin/expect-test.rs
@@ -1,14 +1,17 @@
+use serde::Serialize;
+
 fn main() {}
 
 #[cfg(test)]
-#[ignore]
 mod tests {
+    use crate::AssertJson;
     use expect_test::expect;
+    use serde::Serialize;
 
     #[test]
     fn test_addition() {
         let actual = 2 + 2;
-        let expected = expect!["5"];
+        let expected = expect!["4"];
         expected.assert_eq(&actual.to_string())
     }
 
@@ -22,9 +25,51 @@ mod tests {
         let actual = Foo { value: 92 };
         let expected = expect![[r#"
             Foo {
-                value: 91,
+                value: 92,
             }
         "#]];
         expected.assert_debug_eq(&actual);
+    }
+
+    #[test]
+    fn test_json() {
+        #[derive(Debug, Serialize)]
+        struct Foo {
+            value: i32,
+        }
+
+        let actual = Foo { value: 92 };
+        let expected = expect![[r#"
+            {
+              "value": 92
+            }"#]];
+        expected.assert_json_eq(&actual);
+    }
+}
+
+pub trait AssertJson {
+    #[track_caller]
+    fn assert_json_eq<T>(&self, actual: &T)
+    where
+        T: ?Sized + Serialize;
+}
+
+impl AssertJson for expect_test::Expect {
+    fn assert_json_eq<T>(&self, actual: &T)
+    where
+        T: ?Sized + Serialize,
+    {
+        let actual = serde_json::to_string_pretty(&actual).unwrap();
+        self.assert_eq(&actual)
+    }
+}
+
+impl AssertJson for expect_test::ExpectFile {
+    fn assert_json_eq<T>(&self, actual: &T)
+    where
+        T: ?Sized + Serialize,
+    {
+        let actual = serde_json::to_string_pretty(&actual).unwrap();
+        self.assert_eq(&actual)
     }
 }
